deb142e09d47b27d9a56077e70a9937e7b45266c
Per Inge Mathisen <per.mathisen@arm.com>
2025-05-15 15:05:09 +0200

    Add startup scripts

---
+++ update-artifacts.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+touch .cached
+mkdir -p artifacts
+
+export TRACEUI_LATEST_RELEASE="r1p0-pre"
+export TRACEUI_CORE_URL="https://github.com/ARM-software/traceui/releases/download"
+export TRACEUI_CACHED_VERSION=$(cat .cached)
+
+if [[ "${TRACEUI_LATEST_RELEASE}" != "$TRACEUI_CACHED_VERSION" ]]; then
+	echo "Updating artifacts for ${TRACEUI_LATEST_RELEASE}..."
+
+	wget $TRACEUI_CORE_URL/${TRACEUI_LATEST_RELEASE}/gfxreconstruct-arm.tar.gz
+	wget $TRACEUI_CORE_URL/${TRACEUI_LATEST_RELEASE}/hwcpipe.tar.gz
+	wget $TRACEUI_CORE_URL/${TRACEUI_LATEST_RELEASE}/patrace.tar.gz
+
+	tar xzf gfxreconstruct-arm.tar.gz -C artifacts
+	tar xzf hwcpipe.tar.gz -C artifacts
+	tar xzf patrace.tar.gz -C artifacts
+	rm -f gfxreconstruct-arm.tar.gz hwcpipe.tar.gz patrace.tar.gz
+
+	echo "${TRACEUI_LATEST_RELEASE}" >> .cached
+	echo "... done."
+fi
