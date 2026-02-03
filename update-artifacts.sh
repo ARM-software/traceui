0ce07adfc485973c57e86d0bb4c8e6ba2ebfb164
Edem Avevor <edem.avevor@arm.com>
2026-02-03 15:21:05 +0100

    [gfxr r4p2] Update release for gfxr
    Change-Id: I72b5e0def3e76c4c9369a5d824754718fb69d10c

--- update-artifacts.sh
+++ update-artifacts.sh
@@ -10,21 +10,21 @@ export TRACEUI_LATEST_RELEASE="r1p1"
 export TRACEUI_CORE_URL="https://github.com/ARM-software/traceui/releases/download"
 export TRACEUI_CACHED_VERSION=$(cat .cached)
 
-GFXRECON_RELEASE="r4p1"
+GFXRECON_RELEASE="r4p2"
 PATRACE_RELEASE="r5p4"
 
 if [[ "${TRACEUI_LATEST_RELEASE}" != "$TRACEUI_CACHED_VERSION" ]]; then
-	echo "Updating artifacts for ${TRACEUI_LATEST_RELEASE}..."
+  echo "Updating artifacts for ${TRACEUI_LATEST_RELEASE}..."
 
-	wget $TRACEUI_CORE_URL/${TRACEUI_LATEST_RELEASE}/gfxreconstruct-arm-${GFXRECON_RELEASE}.tar.gz
-	wget $TRACEUI_CORE_URL/${TRACEUI_LATEST_RELEASE}/hwcpipe.tar.gz
-	wget $TRACEUI_CORE_URL/${TRACEUI_LATEST_RELEASE}/patrace-${PATRACE_RELEASE}.tar.gz
+  wget $TRACEUI_CORE_URL/${TRACEUI_LATEST_RELEASE}/gfxreconstruct-arm-${GFXRECON_RELEASE}.tar.gz
+  wget $TRACEUI_CORE_URL/${TRACEUI_LATEST_RELEASE}/hwcpipe.tar.gz
+  wget $TRACEUI_CORE_URL/${TRACEUI_LATEST_RELEASE}/patrace-${PATRACE_RELEASE}.tar.gz
 
-	tar xzf gfxreconstruct-arm-${GFXRECON_RELEASE}.tar.gz -C artifacts
-	tar xzf hwcpipe.tar.gz -C artifacts
-	tar xzf patrace-${PATRACE_RELEASE}.tar.gz -C artifacts
-	rm -f gfxreconstruct-arm-${GFXRECON_RELEASE}.tar.gz hwcpipe.tar.gz patrace-${PATRACE_RELEASE}.tar.gz
+  tar xzf gfxreconstruct-arm-${GFXRECON_RELEASE}.tar.gz -C artifacts
+  tar xzf hwcpipe.tar.gz -C artifacts
+  tar xzf patrace-${PATRACE_RELEASE}.tar.gz -C artifacts
+  rm -f gfxreconstruct-arm-${GFXRECON_RELEASE}.tar.gz hwcpipe.tar.gz patrace-${PATRACE_RELEASE}.tar.gz
 
-	echo "${TRACEUI_LATEST_RELEASE}" > .cached
-	echo "... done."
+  echo "${TRACEUI_LATEST_RELEASE}" >.cached
+  echo "... done."
 fi
